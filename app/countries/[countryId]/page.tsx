03b6f5e67c5996ef62e1617604d14389b47f2e04
sonacho <nachokajita@gmail.com>
2023-10-02 21:53:48 -0300

    some changes

--- app/countries/[countryId]/page.tsx
+++ app/countries/[countryId]/page.tsx
@@ -1,21 +1,38 @@
 import getCountry from "@/actions/get-country";
 import CordsToMap from "@/components/cords-to-map";
-import { ArrowLeftCircle } from "lucide-react";
+import { ArrowLeftCircle, FrownIcon } from "lucide-react";
 
 import Link from "next/link";
 
 const CountryDetail = async ({ params }: { params: { countryId: string } }) => {
   const country = await getCountry(params.countryId);
 
+  if(!country){
+    return (
+      <div className="h-screen w-screen flex flex-col gap-y-3 items-center justify-center">
+        <h1 className="text-3xl text-neutral-300 flex items-center">There is no country with the searched ID &nbsp;<span><FrownIcon size={30}/></span></h1>
+        <Link
+              href={"/countries"}
+              className="rounded-md z-10 w-30 h-10 text-center flex items-center p-5 mt-3 bg-[#202C37] text-neutral-200 shadow-md shadow-slate-900"
+            >
+              <span className="flex">
+                <ArrowLeftCircle />
+                Back
+              </span>
+            </Link>
+      </div>
+    )
+  }
+
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className=" bg-[#2B3945] w-11/12 h-5/6 rounded-lg flex flex-col">
-        <div className="w-full h-[80%] flex justify-around p-10">
-          <div className="w-1/2 h-full flex items-center justify-center">
+        <div className="w-full h-[70%] flex justify-around p-10">
+          <div className="w-1/2 h-full flex items-center justify-center p-10 overflow-hidden">
           <img
             src={country.img}
             alt="flag"
-            className="rounded-lg max-h-full"
+            className="rounded-lg h-full object-cover"
           />
           </div>
           <div className="flex flex-col grow items-center justify-center">
